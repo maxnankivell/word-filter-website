116d70743ec13f0cda4da4dfe56f44fad7714547
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2022-06-07 00:31:56 +1200

    feat: add firestore db connection

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <h1>word</h1>
+  <h1>words</h1>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss"></style>
---
+++ src/firestore.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJMIzcJd1UNemPjhv7-2Me1647Pki0O2I",
+  authDomain: "word-filter-website.firebaseapp.com",
+  projectId: "word-filter-website",
+  storageBucket: "word-filter-website.appspot.com",
+  messagingSenderId: "420803164071",
+  appId: "1:420803164071:web:c37f404f74a3ce002ea882",
+  measurementId: "G-T8749Q3NK7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db;
